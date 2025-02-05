f14539034c8a54b6d34936dd336368f500e422ba
eddaniel01 <edrb0001@ce.pucmm.edu.do>
2025-02-05 12:34:15 -0400

    falta estilizar vista y botones

---
+++ practica-2/practica-2/src/main/java/org/example/Estudiante.java
@@ -0,0 +1,27 @@
+package org.example;
+public class Estudiante {
+    private String matricula; // Será el identificador único
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public Estudiante(String matricula, String nombre, String apellido, String telefono) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    // Getters y Setters
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+}
