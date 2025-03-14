c331fcc5a0602160a4f353cf5cecc6f60baeb523
eddaniel01 <edrb0001@ce.pucmm.edu.do>
2025-03-14 19:04:23 -0400

    app base

--- practica-6/practica-6/src/main/java/com/ejemplo/practica6/MyBeansConfig.java
+++ practica-6/practica-6/src/main/java/com/ejemplo/practica6/MyBeansConfig.java
@@ -22,6 +22,7 @@ public class MyBeansConfig {
         return localeResolver;
     }
 
+
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
