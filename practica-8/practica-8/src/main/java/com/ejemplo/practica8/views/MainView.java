bb53f267ccc89a33d6fcd35aec07a5bcdf486494
eddaniel01 <edrb0001@ce.pucmm.edu.do>
2025-04-14 14:10:41 -0400

    no hay que refrecar la vista para mostrar contenido

--- practica-8/practica-8/src/main/java/com/ejemplo/practica8/views/MainView.java
+++ practica-8/practica-8/src/main/java/com/ejemplo/practica8/views/MainView.java
@@ -1,25 +1,27 @@
 package com.ejemplo.practica8.views;
 
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.*;
 import com.vaadin.flow.server.VaadinSession;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
+import jakarta.annotation.security.PermitAll;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Route("")
-@AnonymousAllowed
-public class MainView extends VerticalLayout {
+@PageTitle("Inicio")
+@PermitAll // ← Acepta a usuarios autenticados y anónimos
+public class MainView extends VerticalLayout implements AfterNavigationObserver {
 
-    public MainView() {
-        if (SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
-                && !(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)) {
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    @Override
+    public void afterNavigation(AfterNavigationEvent event) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-            VaadinSession.getCurrent().getSession().removeAttribute("SPRING_SECURITY_SAVED_REQUEST");
+        VaadinSession.getCurrent().getSession().removeAttribute("SPRING_SECURITY_SAVED_REQUEST");
+
+        if (auth != null && auth.isAuthenticated() &&
+                !(auth instanceof AnonymousAuthenticationToken)) {
 
             if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
                 UI.getCurrent().navigate("gerentes");
@@ -32,4 +34,3 @@ public class MainView extends VerticalLayout {
         }
     }
 }
-
